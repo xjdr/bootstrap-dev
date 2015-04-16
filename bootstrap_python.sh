98d2b16b73bd283e35d669c11edf533fd422acaf
Jeff Rose <jeff.rose12@gmail.com>
2015-04-16 21:24:10 +0000

    fixed bootstrap_python

--- bootstrap_python.sh
+++ bootstrap_python.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-INSTALL_ROOT=$HOME/Python
+INSTALL_ROOT=/opt/python
 CPY=$INSTALL_ROOT/CPython
 PYPY=$INSTALL_ROOT/PyPy
 
-SANDBOX=$(mktemp -d /tmp/python.XXXXXX)
+SANDBOX=/tmp/python-sandbox
 
-CURL='wget --no-check-certificate'
+CURL='curl -k'
 
-mkdir -p $INSTALL_ROOT
+if [[ ! -d $INSTALL_ROOT ]]
+then
+  sudo mkdir -p $INSTALL_ROOT
+fi
+
+if [[ ! -d $SANDBOX ]]
+then
+  sudo mkdir -p $SANDBOX
+fi
+
+sudo chown $(whoami): $INSTALL_ROOT
+sudo chown $(whoami): $SANDBOX
 
 pushd $SANDBOX
-  wget ftp://ftp.cwru.edu/pub/bash/readline-6.2.tar.gz
-  tar xzf readline-6.2.tar.gz
-  pushd readline-6.2
-    ./configure --disable-shared --enable-static --prefix=$SANDBOX/readline
-    make -j3 && make install
-  popd
-  rm -rf readline-6.2.tar.gz readline-6.2
-
-  # install all major cpython interpreter versions
-  for version in 2.6.9 2.7.8 3.3.5 3.4.1; do
-    $CURL http://python.org/ftp/python/$version/Python-$version.tgz
+  for version in 2.6.9 2.7.9 3.4.3; do
+    $CURL https://www.python.org/ftp/python/$version/Python-$version.tgz -o Python-$version.tgz
+    rc=$?
+    if [[ $rc != 0 ]]
+    then
+      exit
+    fi
     tar xzf Python-$version.tgz
     pushd Python-$version
       LDFLAGS=-L$SANDBOX/readline/lib CFLAGS=-I$SANDBOX/readline/include \
@@ -30,26 +37,24 @@ pushd $SANDBOX
     rm -f Python-$version.tgz
   done
 
-  # install pypy
-  for pypy_version in 2.2.1-osx64; do
-    pushd $INSTALL_ROOT
-      $CURL https://bitbucket.org/pypy/pypy/downloads/pypy-$pypy_version.tar.bz2
-      bzip2 -cd pypy-$pypy_version.tar.bz2 | tar -xf -
-      rm -f pypy-$pypy_version.tar.bz2
-      mv pypy-$pypy_version PyPy-2.2.1
-    popd
-  done
-
-  $CURL https://pypi.python.org/packages/source/s/setuptools/setuptools-3.4.4.tar.gz
-  $CURL http://pypi.python.org/packages/source/p/pip/pip-1.5.4.tar.gz
+  $CURL https://pypi.python.org/packages/source/s/setuptools/setuptools-15.1.tar.gz -o setuptools-15.1.tar.gz
+  rc=$?
+  if [[ $rc != 0 ]]
+  then
+    exit
+  fi
+  $CURL https://pypi.python.org/packages/source/p/pip/pip-6.1.1.tar.gz -o pip-6.1.1.tar.gz
+  rc=$?
+  if [[ $rc != 0 ]]
+  then
+    exit
+  fi
 
   for interpreter in $CPY-2.6.9/bin/python2.6 \
-                     $CPY-2.7.8/bin/python2.7 \
-                     $CPY-3.3.5/bin/python3.3 \
-                     $CPY-3.4.1/bin/python3.4 \
-                     $PYPY-2.2.1/bin/pypy; do
+                     $CPY-2.7.9/bin/python2.7 \
+                     $CPY-3.4.3/bin/python3.4; do
     # install distribute && pip
-    for base in setuptools-3.4.4 pip-1.5.4; do
+    for base in setuptools-15.1 pip-6.1.1; do
       tar xzf $base.tar.gz
       pushd $base
         $interpreter setup.py install
@@ -58,16 +63,16 @@ pushd $SANDBOX
     done
   done
 
-  rm -f setuptools-3.4.4.tar.gz pip-1.5.4.tar.gz
+  rm -f setuptools-15.1.tar.gz pip-6.1.1.tar.gz
 popd
 
 METAPATH='$PATH'
 for path in $(ls $INSTALL_ROOT | sort -r); do
-  METAPATH=$INSTALL_ROOT/$path/bin:$METAPATH
+  echo -ne 'export PYTHON_HOME='$INSTALL_ROOT'/'$path'\n' >> ${HOME}'/.python_home'
 done
 
-echo Add the following line to the end of your .bashrc:
-echo PATH=$METAPATH
+## Python Home
+#[[ -s ${HOME}/.python_home ]] && source ${HOME}/.python_home
 
 rm -rf $SANDBOX
 
